ef73e1d9ce23ccb3a4ac91ded37f8a33b5620e7a
bao <dangquocbao13170@gmail.com>
2020-06-04 15:11:54 +0700

    part 2

---
+++ dangquocbao_lab456/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dangquocbao_lab456.MvcApplication" Language="C#" %>
---
+++ dangquocbao_lab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dangquocbao_lab456.Startup))]
+namespace dangquocbao_lab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
