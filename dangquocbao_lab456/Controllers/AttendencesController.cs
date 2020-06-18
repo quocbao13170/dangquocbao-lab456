635227ec50beae026e202e5ad999cd833564f775
Dangquocbao <dangquocbao13170@gmail.com>
2020-06-18 15:54:36 +0700

    diem danh chieu thu 5 (18/6/2020)

--- dangquocbao_lab456/Controllers/AttendencesController.cs
+++ dangquocbao_lab456/Controllers/AttendencesController.cs
@@ -1,4 +1,5 @@
 ﻿using dangquocbao_lab456.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,13 +13,17 @@ namespace dangquocbao_lab456.Controllers
     public class AttendencesController : ApiController
     {
         private ApplicationDbContext _dbContext;
-    }
-    public IHttpActionResult Attend([FromBody] int courseId)
-    {
-        var attendance = new Attendance
+        public IHttpActionResult Attend([FromBody] int courseId)
         {
-            CourseId = courseId,
-            AttendeeId = User.Indentity.GetUserId()
-        };
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
     }
+    
 }
